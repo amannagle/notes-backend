e05d39e0842134aa1b94ba45fb0f1e41d486dee8
amannagle <aman.nagle1999@gmail.com>
2024-06-12 20:32:38 +0530

    react file added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const cors = require('cors');
 const app = express()
+app.use(express.static('dist'))
 app.use(cors());
 app.use(express.json());
 let notes = [
